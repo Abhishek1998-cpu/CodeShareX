1d133529db8011f9afef620321fbe2b08b53a34e
Abhishek Verma <abhishekverma998@gmail.com>
2022-08-15 22:34:10 +0530

    testing for other compiler

--- server/generateFile.js
+++ server/generateFile.js
@@ -1,21 +1,23 @@
 const fs = require("fs")
 const path = require("path")
 const dirCodes = path.join(__dirname, "codes")
-const {v4: uuidv4} = require("uuid")
+const { v4: uuidv4 } = require("uuid")
 
 const generateFile = async (format, content) => {
-    const jobId = uuidv4()
-    const fileName = `${jobId}.${format}`
-    const filePath = path.join(dirCodes, fileName)
-    await fs.writeFileSync(filePath, content)
-    return filePath
-
+  const jobId = uuidv4()
+  const fileName = `${jobId}.${format}`
+  console.log(fileName)
+  const filePath = path.join(dirCodes, fileName)
+  console.log(filePath)
+  //   Here
+  await fs.writeFileSync(filePath, content)
+  return filePath
 }
 
-if(!fs.existsSync(dirCodes)){
-    fs.mkdirSync(dirCodes, {recursive: true})
+if (!fs.existsSync(dirCodes)) {
+  fs.mkdirSync(dirCodes, { recursive: true })
 }
 
 module.exports = {
-    generateFile
+  generateFile,
 }
--- server/server.js
+++ server/server.js
@@ -1,24 +1,24 @@
-const ACTIONS = require("../src/Actions");
-const express = require("express");
-const App = express();
-const http = require("http").createServer(App);
-const mongoose = require("mongoose");
-const { generateFile } = require("./generateFile");
-const { executeCpp } = require("./executeCpp");
-const { executePy } = require("./executePy");
-const { executeJs } = require("./executeJs");
-const { executeC } = require("./executeC");
-const Job = require("./models/Job");
-let cors = require("cors");
+const ACTIONS = require("../src/Actions")
+const express = require("express")
+const App = express()
+const http = require("http").createServer(App)
+const mongoose = require("mongoose")
+const { generateFile } = require("./generateFile")
+const { executeCpp } = require("./executeCpp")
+const { executePy } = require("./executePy")
+const { executeJs } = require("./executeJs")
+const { executeC } = require("./executeC")
+const Job = require("./models/Job")
+let cors = require("cors")
 
 const io = require("socket.io")(http, {
   cors: {
     origins: ["http://localhost:8080/"],
   },
-});
+})
 
 const DB =
-  "mongodb+srv://Abhishek:000@cluster0.dg7dbtp.mongodb.net/codesharex?retryWrites=true&w=majority";
+  "mongodb+srv://Abhishek:000@cluster0.dg7dbtp.mongodb.net/codesharex?retryWrites=true&w=majority"
 
 mongoose
   .connect(DB, {
@@ -28,95 +28,101 @@ mongoose
     // useFindAndModify: false,
   })
   .then(() => {
-    console.log("Connection Successfull");
+    console.log("Connection Successfull")
   })
   .catch((err) => {
-    console.log("Connection Unsuccessful: " + err);
-  });
+    console.log("Connection Unsuccessful: " + err)
+  })
 
-App.use(express.urlencoded({ extended: true }));
-App.use(express.json());
-App.use(cors());
+App.use(express.urlencoded({ extended: true }))
+App.use(express.json())
+App.use(cors())
 
-const userSocketMap = {};
+const userSocketMap = {}
 function getAllConnectedClients(roomId) {
   return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
     (socketId) => {
       return {
         socketId,
         userName: userSocketMap[socketId],
-      };
+      }
     }
-  );
+  )
 }
 
 App.get("/", (req, res) => {
-  res.send("<h2>Welcome to the Live Code Share</h2>");
-});
+  res.send("<h2>Welcome to the Live Code Share</h2>")
+})
 
 App.get("/status", async (req, res) => {
-  const jobId = req.query.id;
+  const jobId = req.query.id
   if (jobId === undefined) {
     return res
       .status(400)
-      .json({ success: false, error: "Missing Id Query Params " });
+      .json({ success: false, error: "Missing Id Query Params " })
   }
   // console.log(jobId);
   try {
-    const job = await Job.findById(jobId);
+    const job = await Job.findById(jobId)
     if (job === undefined) {
-      return res.status(404).json({ success: false, error: "Invalid Job Id" });
+      return res.status(404).json({ success: false, error: "Invalid Job Id" })
     }
-    return res.status(200).json({ success: true, job });
+    return res.status(200).json({ success: true, job })
   } catch (error) {
     return res
       .status(400)
-      .json({ success: false, error: JSON.stringify(error) });
+      .json({ success: false, error: JSON.stringify(error) })
   }
-});
+})
 
 App.post("/run", async (req, res) => {
-  const { language = "cpp", code } = req.body;
+  const { language = "cpp", code } = req.body
+  console.log(language)
+  console.log(code)
   if (code === undefined) {
     return res
       .status(400)
-      .json({ success: false, error: "Empty! Code not found" });
+      .json({ success: false, error: "Empty! Code not found" })
   }
-  let job;
+  let job
   // console.log(language);
   // console.log(code);
   try {
-    const filePath = await generateFile(language, code);
-    job = await new Job({ language, filePath }).save();
-    const jobId = job["_id"];
-    res.status(201).json({ success: true, jobId });
-    console.log("New 5 = " + job);
-    let output;
-    job["startedAt"] = new Date();
+    const filePath = await generateFile(language, code)
+    job = await new Job({ language, filePath }).save()
+    const jobId = job["_id"]
+    res.status(201).json({ success: true, jobId })
+    // console.log("New 5 = " + job)
+    let output
+    job["startedAt"] = new Date()
     if (language === "py") {
-      output = await executePy(filePath);
+      // console.log(filePath)
+      return "Hello World From Py"
+      // output = await executePy(filePath)
     } else if (language === "js") {
-      output = await executeJs(filePath);
-    } else if(language === "c"){
+      // console.log(filePath)
+      return "Hello World From Js"
+      // output = await executeJs(filePath)
+    } else if (language === "c") {
       output = await executeC(filePath)
-    }else{
-      output = await executeCpp(filePath);
+    } else {
+      output = await executeCpp(filePath)
     }
-    job["completedAt"] = new Date();
-    job["status"] = "success";
-    job["output"] = output;
-    await job.save();
+    job["completedAt"] = new Date()
+    job["status"] = "success"
+    job["output"] = output
+    await job.save()
     // console.log({ filePath, output });
     // return res.status(200).json({ filePath, output });
   } catch (err) {
-    job["completedAt"] = new Date();
-    job["status"] = "error";
-    job["output"] = JSON.stringify(err);
-    await job.save();
+    job["completedAt"] = new Date()
+    job["status"] = "error"
+    job["output"] = JSON.stringify(err)
+    await job.save()
     // console.log(job);
     // res.status(500).json({ err });
   }
-});
+})
 
 io.on("connection", (socket) => {
   // console.log("Socket Connected", socket.id);
@@ -125,9 +131,9 @@ io.on("connection", (socket) => {
   // });
   socket.on(ACTIONS.JOIN, ({ roomId, userName }) => {
     // console.log("User joined successfully");
-    userSocketMap[socket.id] = userName;
-    socket.join(roomId);
-    const clients = getAllConnectedClients(roomId);
+    userSocketMap[socket.id] = userName
+    socket.join(roomId)
+    const clients = getAllConnectedClients(roomId)
     // console.log("New 4 = " + JSON.stringify(clients));
     // console.log(ACTIONS.JOINED);
     clients.forEach(({ socketId }) => {
@@ -135,36 +141,36 @@ io.on("connection", (socket) => {
         clients,
         userName,
         socketId: socket.id,
-      });
-    });
-  });
+      })
+    })
+  })
 
   socket.on(ACTIONS.CODE_CHANGE, ({ roomId, code }) => {
     // console.log("Working again", code);
-    socket.in(roomId).emit(ACTIONS.CODE_CHANGE, { code });
-  });
+    socket.in(roomId).emit(ACTIONS.CODE_CHANGE, { code })
+  })
 
   socket.on(ACTIONS.SYNC_CODE, ({ code, socketId }) => {
     // console.log("Code = " + code);
     // console.log("SocketId = " + socketId);
-    io.to(socketId).emit(ACTIONS.CODE_CHANGE, { code });
-  });
+    io.to(socketId).emit(ACTIONS.CODE_CHANGE, { code })
+  })
 
   socket.on("disconnecting", () => {
-    const rooms = [...socket.rooms];
+    const rooms = [...socket.rooms]
     rooms.forEach((roomId) => {
       socket.in(roomId).emit(ACTIONS.DISCONNECTED, {
         socketId: socket.id,
         userName: userSocketMap[socket.id],
-      });
-    });
-    delete userSocketMap[socket.id];
-    socket.leave();
-  });
-});
+      })
+    })
+    delete userSocketMap[socket.id]
+    socket.leave()
+  })
+})
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
 http.listen(PORT, () => {
-  console.log(`Listening on the port ${PORT}`);
-});
+  console.log(`Listening on the port ${PORT}`)
+})
